1de3197190b6b35b28a160988674aab1b32ba87d
stelstel <sthlm_32@hotmail.com>
2020-09-01 19:17:39 +0200

    Add project files.

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ovningssamling_Inkapsling_arv_o_polymorfism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
