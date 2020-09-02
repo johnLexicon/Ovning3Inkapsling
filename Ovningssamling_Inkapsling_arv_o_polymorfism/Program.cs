dd577e177c32befdbb7e52f3dfa9390d792dc6f3
stelstel <sthlm_32@hotmail.com>
2020-09-02 15:29:08 +0200

    Commented out instance of Personfrom Program.cs Made some persons through class Person Handler and tested some methods from class PersonHandler

--- Ovningssamling_Inkapsling_arv_o_polymorfism/Program.cs
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Program.cs
@@ -6,6 +6,7 @@ namespace OvningssamlingInkapslingArvOchPolymorfism
     {
         static void Main(string[] args)
         {
+            /*
             Person person = null;
 
             try
@@ -17,15 +18,23 @@ namespace OvningssamlingInkapslingArvOchPolymorfism
             {
                 
             }
+            */
 
-            PersonHandler.SetAge(person, 53);
+            /*PersonHandler.SetAge(person, 53);
             Console.WriteLine(person.ToString()); // TODO ///////////////
+            */
 
             Person person2 = PersonHandler.CreatePerson(10, "Lisa", "Karlsson", 1.40, 35);
             Console.WriteLine(person2.ToString()); // TODO ///////////////
 
             PersonHandler.SetFirstName(person2, "Gittan");
             Console.WriteLine(person2.ToString()); // TODO Testutskrift ///////////////
+
+            Person person3 = PersonHandler.CreatePerson(13, "Arne", "Olsson", 1.75, 81);
+            Console.WriteLine(person3.ToString()); // TODO ///////////////
+
+            PersonHandler.SetWeight(person3, 83);
+            Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
         }
     }
 }
