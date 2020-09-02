4c1f04154e0791fea0cc4f542a19be17f2dcd778
stelstel <sthlm_32@hotmail.com>
2020-09-02 15:18:56 +0200

    Added set-methods

--- Ovningssamling_Inkapsling_arv_o_polymorfism/PersonHandler.cs
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/PersonHandler.cs
@@ -6,9 +6,29 @@ namespace OvningssamlingInkapslingArvOchPolymorfism
 {
     public static class PersonHandler
     {
+        public static void SetFirstName(Person pers, string firstName)
+        {
+            pers.FName = firstName;
+        }
+
+        public static void SetLastName(Person pers, string lastName)
+        {
+            pers.LName = lastName;
+        }
+
         public static void SetAge(Person pers, int age)
         {
             pers.Age = age;
+        }
+
+        public static void SetHeight(Person pers, int height)
+        {
+            pers.Height = height;
+        }
+
+        public static void SetWeight(Person pers, int weight)
+        {
+            pers.Weight = weight;
         }
 
         public static Person CreatePerson(int age, string fname, string lname, double height, double weight)
