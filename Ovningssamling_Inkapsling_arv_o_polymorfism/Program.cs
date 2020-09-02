02deae9ab67ad30c92b4ca2320cda4de543e6164
stelstel <sthlm_32@hotmail.com>
2020-09-02 23:34:34 +0200

    -

--- Ovningssamling_Inkapsling_arv_o_polymorfism/Program.cs
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Program.cs
@@ -8,18 +8,6 @@ namespace OvnSamlingInkapslArvOPolym
     {
         static void Main(string[] args)
         {
-            /*
-            Bird bird = new Bird();
-            bird.Name = "Berit";
-            bird.Age = 77;
-            bird.Weight = 5;
-            bird.WingSpan = 3;
-
-            Console.WriteLine("Bird Stats:");
-            Console.WriteLine(bird.Stats());
-            Console.WriteLine();
-            */
-
             List<Animal> animals = new List<Animal>();
 
             animals.Add(new Pelican()
@@ -98,7 +86,7 @@ namespace OvnSamlingInkapslArvOPolym
                 {
                     Console.WriteLine(animal.Stats());
                 }
-                
+
             }
 
             List<Dog> dogs = new List<Dog>();
@@ -127,45 +115,27 @@ namespace OvnSamlingInkapslArvOPolym
                 Iq = 35
             });
 
-            /*
+            // testingPerson();
+        }
+
+        private static void testingPerson()
+        {
             Person person = null;
 
             try
             {
                 person = new Person("Stefan", "Elmgren");
-                Console.WriteLine(person.ToString()); // TODO ///////////////
             }
-            catch (Exception)
-            {
-                
-            }
-            */
+            catch (Exception) { }
 
-            /*
-             * PersonHandler.SetAge(person, 53);
-            Console.WriteLine(person.ToString()); // TODO ///////////////
-            */
-
+            PersonHandler.SetAge(person, 53);
             Person person2 = PersonHandler.CreatePerson(10, "Lisa", "Karlsson", 140, 35);
-            //Console.WriteLine(person2.ToString()); // TODO Testutskrift ///////////////
-
             PersonHandler.SetFirstName(person2, "Gittan");
-            //Console.WriteLine(person2.ToString()); // TODO Testutskrift ///////////////
-
             Person person3 = PersonHandler.CreatePerson(13, "Arne", "Olsson", 175, 81);
-            //Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
-
             PersonHandler.SetWeight(person3, 83);
-            //Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
-
             PersonHandler.SetLastName(person3, "Larsson");
-            //Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
-
             PersonHandler.SetHeight(person3, 165);
-            //Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
-
             PersonHandler.SetAge(person3, 14);
-            //Console.WriteLine(person3.ToString()); // TODO Testutskrift ///////////////
         }
     }
 }
