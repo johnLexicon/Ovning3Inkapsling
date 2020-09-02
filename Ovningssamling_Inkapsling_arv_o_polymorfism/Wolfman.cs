dba4a61cbd965058374957e63afbd0c5da64db90
stelstel <sthlm_32@hotmail.com>
2020-09-02 17:08:55 +0200

    Added class Wolfman

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Wolfman.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OvningssamlingInkapslingArvOchPolymorfism
+{
+    class Wolfman : Wolf, IPerson
+    {
+        public void Talk() 
+        {
+        
+        }
+    }
+}
