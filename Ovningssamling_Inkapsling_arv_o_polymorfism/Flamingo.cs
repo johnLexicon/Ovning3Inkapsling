8d395e644caf843a21b8fa3ec6d6b22817b971f1
stelstel <sthlm_32@hotmail.com>
2020-09-02 17:02:21 +0200

    Added 3 brird classes

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Flamingo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OvningssamlingInkapslingArvOchPolymorfism
+{
+    class Flamingo : Bird
+    {
+        public int LegLength { get; set; }
+    }
+}
---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/Swan.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OvningssamlingInkapslingArvOchPolymorfism
+{
+    class Swan : Bird
+    {
+        public int NeckLength { get; set; }
+    }
+}
