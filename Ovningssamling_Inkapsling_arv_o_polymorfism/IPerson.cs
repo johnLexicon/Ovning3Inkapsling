1444fa690ef26475a12a5a8a0db64cd34abd4c90
stelstel <sthlm_32@hotmail.com>
2020-09-02 17:05:33 +0200

    Added interface IPerson

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/IPerson.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OvningssamlingInkapslingArvOchPolymorfism
+{
+    interface IPerson
+    {
+        void Talk();
+    }
+}
