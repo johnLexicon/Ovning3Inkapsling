8a8fb4af793e2a3e84be2130012ed959b6e8bd29
stelstel <sthlm_32@hotmail.com>
2020-09-03 20:19:51 +0200

    Added class TextInputError

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/TextInputError.cs
@@ -0,0 +1,15 @@
+﻿using OvnSamlingInkapslArvOPolym;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3
+{
+    public class TextInputError : UserError
+    {
+        public override string UEMessage() 
+        {
+            return "You tried to use a text input in a numericonly field.This fired an error!";
+        }
+    }
+}
