d6e36d2de47678b2b5220cf1ad8afdac102e7825
stelstel <sthlm_32@hotmail.com>
2020-09-03 17:55:01 +0200

    Added class UserError

---
+++ Ovningssamling_Inkapsling_arv_o_polymorfism/UserError.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OvnSamlingInkapslArvOPolym
+{
+    abstract class UserError
+    {
+        public abstract string UEMessage();
+    }
+}
