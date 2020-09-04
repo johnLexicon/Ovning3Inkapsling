c3da9ca42747fb7ea5f1dd3fce98074aefdbe038
stelstel <sthlm_32@hotmail.com>
2020-09-04 18:30:36 +0200

    Added comments to interface IUI

--- Ovning3/IUI.cs
+++ Ovning3/IUI.cs
@@ -2,8 +2,22 @@
 {
     interface IUI
     {
+        /// <summary>
+        /// Read input
+        /// </summary>
+        /// <returns>input</returns>
         string GetInput();
+
+        /// <summary>
+        /// Prints string
+        /// </summary>
+        /// <param name="message">string to print</param>
         void Print(string message);
+
+        /// <summary>
+        /// Prints string ending with line break
+        /// </summary>
+        /// <param name="message">string to print</param>
         void PrintLine(string message = "");
     }
 }
