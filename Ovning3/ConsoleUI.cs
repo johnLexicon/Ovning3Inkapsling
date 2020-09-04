c3bd43dab5cedb75d724f5a0ea85ad01f07618d2
stelstel <sthlm_32@hotmail.com>
2020-09-04 12:33:59 +0200

    Commented ConsoleUI

--- Ovning3/ConsoleUI.cs
+++ Ovning3/ConsoleUI.cs
@@ -6,16 +6,28 @@ namespace Ovning3
 {
     public class ConsoleUI : IUI
     {
+        /// <summary>
+        /// Read input from Console
+        /// </summary>
+        /// <returns>User input</returns>
         public string GetInput()
         {
             return Console.ReadLine();
         }
 
+        /// <summary>
+        /// Prints string to Console
+        /// </summary>
+        /// <param name="message">string to print</param>
         public void Print(string message)
         {
             Console.Write(message);
         }
 
+        /// <summary>
+        /// Prints string to Console ending with line break
+        /// </summary>
+        /// <param name="message">string to print</param>
         public void PrintLine(string message)
         {
             Console.WriteLine(message);
