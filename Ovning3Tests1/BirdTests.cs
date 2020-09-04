faf58aa75e845e780445fe641d3c0a4f4054a674
stelstel <sthlm_32@hotmail.com>
2020-09-04 15:42:38 +0200

    Added class BirdTests

---
+++ Ovning3Tests1/BirdTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ovning3;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ovning3.Tests
+{
+    [TestClass()]
+    public class BirdTests
+    {
+        [TestMethod()]
+        public void StatsTest()
+        {
+            // Arrange
+            Bird bird = new Bird();
+            int expectedAge         = bird.Age      = 1;
+            string expectedName     = bird.Name     = "Pippi";
+            int expectedWeight      = bird.Weight   = 2;
+            int expectedWingSpan    = bird.WingSpan = 5;
+
+            // Act
+            int actualAge       = bird.Age;
+            string actualName      = bird.Name;
+            int actualWeight    = bird.Weight;
+            int actualWingSpan  = bird.WingSpan;
+
+            // Assert
+            Assert.AreEqual(expectedAge, actualAge);
+            Assert.AreEqual(expectedName, actualName);
+            Assert.AreEqual(expectedWeight, actualWeight);
+            Assert.AreEqual(expectedWingSpan, actualWingSpan);
+        }
+    }
+}
