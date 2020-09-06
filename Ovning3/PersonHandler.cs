336586d8ffdc92e50ce340f4a96fb1d385545480
stelstel <sthlm_32@hotmail.com>
2020-09-06 14:18:56 +0200

    Added null check to setters

--- Ovning3/PersonHandler.cs
+++ Ovning3/PersonHandler.cs
@@ -8,27 +8,43 @@ namespace Ovning3
     {
         public static void SetFirstName(Person pers, string firstName)
         {
-            pers.FName = firstName;
+            if (pers != null)
+            {
+                pers.FName = firstName;
+            }
+            
         }
 
         public static void SetLastName(Person pers, string lastName)
         {
-            pers.LName = lastName;
+            if (pers != null)
+            {
+                pers.LName = lastName;
+            }
         }
 
         public static void SetAge(Person pers, int age)
         {
-            pers.Age = age;
+            if (pers != null)
+            {
+                pers.Age = age;
+            }
         }
 
         public static void SetHeight(Person pers, int height)
         {
-            pers.Height = height;
+            if (pers != null)
+            {
+                pers.Height = height;
+            }
         }
 
         public static void SetWeight(Person pers, double weight)
         {
-            pers.Weight = weight;
+            if (pers != null)
+            {
+                pers.Weight = weight;
+            }
         }
 
         public static Person CreatePerson(int age, string fname, string lname, int height, double weight)
